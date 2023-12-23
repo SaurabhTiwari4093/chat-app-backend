eae1b0ded831297631b58881ff28427a1e5ddecd
Saurabh Tiwari <86095513+SaurabhTiwari4093@users.noreply.github.com>
2023-12-23 23:54:39 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+app.use(cors);
+
+const server = createServer(app);
+const socketIO = new Server(server, {
+  cors: {
+    origin: "http://localhost:3001",
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Chat application backend!");
+});
+
+socketIO.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("message", (msg) => {
+    socketIO.emit("messageResponse", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
